381e0460a196349cf25d3ca2536861d4739f7435
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-07-07 15:29:47 +0300

    use the ccmod.json manifest

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import { IConfigFile } from './configFile';
 import { IConnection } from './connection';
-import { SocketIoConnector } from './connectors/socketIOConnector';
+import { SocketIoConnector } from './connectors/SocketIOConnector';
 import { Multiplayer } from './multiplayer';
 import { IServer } from './server';
 
@@ -13,7 +13,7 @@ export class MultiplayerConfig {
         https: SocketIoConnector,
     };
 
-    private configPath = 'config/config.json';
+    private configPath = 'config.json';
 
     constructor(configPath?: string) {
         this.modPath = simplify.getMod('multiplayer').baseDirectory;
